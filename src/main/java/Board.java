73c1345601a4102b71dc31295f4e55756a323917
kagorax <kelan360@gmail.com>
2022-05-18 16:44:19 +0100

    board is now functioning

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -3,13 +3,40 @@ public class Board {
     int rows = 5;
     int columns = 5;
     int mines = 5;
+    String [][] board = new String[rows][columns];
 
-    public String[][] boardCreate(){
+    //initialise the board
+    public void initialiseBoard(){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                Tile tileArray[][] = new Tile[i][j]; //instantiate an array of tiles
+            }
+        }
+    }
 
-        String[][] board= new String[rows][columns]; //2D array which will act as the grid of tiles
-        return board;
+    //printing the board
+    public String createBoard(){
+        String strBoard = "";
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (j != columns - 1) {
+                    strBoard += board[i][j] + "|";  //for each column, add a divider
+                } else {
+                    strBoard += board[i][j] + "\n"; //end of each column add a new line
+                }
+            }
+            if (i != rows - 1) {
+                String horizontalDivider = new String(new char[rows-1]).replace("\0", "----+");
+                strBoard += horizontalDivider + "----" + "\n";  //for each row add a new line and divider
+            } else {
+                strBoard += " ";
+            }
+        }
+        return strBoard;
 
     }
+
+
 
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,54 +1,11 @@
-import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello and welcome to Minesweeper!");
-
-        int rows = 6;
-        int columns = 5;
-        int mines = 5;
-
-        int[][] grid= new int[rows][columns]; //2D array which will act as the grid of tiles
-
-
-        //set position of each tile and hides them
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                grid[i][j]=0;
-            }
-        }
-
-       //Assigns a value of '-1' to random positions in the array which will signify a mine
-        for(int x=mines;x>0;x--){
-            int randomColumn = (int)(Math.random() * columns);
-            int randomRow = (int)(Math.random() * rows);
-            grid[randomRow][randomColumn]=-1;
-            new Mine(randomColumn,randomRow,true); //creates a mine object
-        }
-
-        //Determines the number of neighbouring mines
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-
-            }
-        }
-
-        //Prints out all objects
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                System.out.printf(String.valueOf(grid[i][j] + "  "));
-            }
-            System.out.println();
-        }
-
-
-
-
-
-
-
-
-
+        Board board = new Board();
+        board.initialiseBoard();
+        System.out.println(board.createBoard());
     }
 }
